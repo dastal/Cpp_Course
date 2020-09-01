95ded905f9efd2f716b1a5805a61854aad0baa96
gmelektronik <david.stalder@gm-elektronik.swiss>
2020-09-01 14:07:12 +0200

    supplementary commit

--- Cpp_Course/main.cpp
+++ Cpp_Course/main.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "Dave" << endl;
+    double num1, num2;
+    cout << " Enter first number: ";
+    cin >> num1;
+
+    cout << " Enter second number: ";
+    cin >> num2;
+
+    cout << "The result is: " << num1 + num2;
 
     return 0;
 }
