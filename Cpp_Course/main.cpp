eac67cbcfc474303915991e6124c832aea707d86
gmelektronik <david.stalder@gm-elektronik.swiss>
2020-09-01 17:27:44 +0200

    occasional commit

--- Cpp_Course/main.cpp
+++ Cpp_Course/main.cpp
@@ -2,112 +2,23 @@
 
 using namespace std;
 
+
 int main()
 {
-    bool isMale = true;
+    int index = 1;
 
-    if(isMale){
-        cout << "You are a male." << endl;
-    }
-
-
-    bool isMale1 = false;
-
-    if(isMale1){
-        cout << "You are a male." << endl;
-    } else {
-        cout << "You are not male." << endl;
+    while(index <= 5){
+        cout << index << endl;
+        index++;
     }
 
     cout << "\n" << endl;
 
-    bool isMale2 = true;
-    bool isTall = true;
-
-    if(isMale2 && isTall){
-        cout << "You are a tall male." << endl;
-    } else {
-        cout << "You are not male." << endl;
-    }
-
-    cout << "\n" << endl;
-
-    bool isMale3 = false;
-    bool isTall1 = true;
-
-    if(isMale3 && isTall1){
-        cout << "You are a tall male." << endl;
-    } else {
-        cout << "You are not male." << endl;
-    }
-
-    cout << "\n" << endl;
-
-    if(isMale3 || isTall1){
-        cout << "You are a tall male." << endl;
-    } else {
-        cout << "You are not male." << endl;
-    }
-
-    cout << "\n" << endl;
-
-    bool isMale4 = true;
-    bool isTall2 = true;
-
-    if(isMale4 && isTall2){
-        cout << "You are a tall male." << endl;
-    } else if (isMale4 && !isTall2){
-        cout << "You are a short male." << endl;
-    } else if (!isMale4 && isTall2){
-        cout << "You are not a male but you are tall." << endl;
-    } else {
-        cout << "You are not male and not tall." << endl;
-    }
-
-    cout << "\n" << endl;
-
-    bool isMale5 = true;
-    bool isTall3 = false;
-
-    if(isMale5 && isTall3){
-        cout << "You are a tall male." << endl;
-    } else if (isMale5 && !isTall3){
-        cout << "You are a short male." << endl;
-    } else if (!isMale5 && isTall3){
-        cout << "You are not a male but you are tall." << endl;
-    } else {
-        cout << "You are not male and not tall." << endl;
-    }
-
-    cout << "\n" << endl;
-
-    bool isMale6 = false;
-    bool isTall4 = true;
-
-    if(isMale6 && isTall4){
-        cout << "You are a tall male." << endl;
-    } else if (isMale6 && !isTall4){
-        cout << "You are a short male." << endl;
-    } else if (!isMale6 && isTall4){
-        cout << "You are not a male but you are tall." << endl;
-    } else {
-        cout << "You are not male and not tall." << endl;
-    }
-
-    cout << "\n" << endl;
-
-    bool isMale7 = false;
-    bool isTall5 = false;
-
-    if(isMale7 && isTall5){
-        cout << "You are a tall male." << endl;
-    } else if (isMale7 && !isTall5){
-        cout << "You are a short male." << endl;
-    } else if (!isMale7 && isTall5){
-        cout << "You are not a male but you are tall." << endl;
-    } else {
-        cout << "You are not male and not tall." << endl;
-    }
+    int index1 = 1;
+    do{
+        cout << index1 << endl;
+        index1++;
+    }while(index1 <=5);
 
     return 0;
 }
