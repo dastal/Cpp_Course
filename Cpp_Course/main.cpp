4928e975bfa33979e46d51e795fb56a80e037588
gmelektronik <david.stalder@gm-elektronik.swiss>
2020-09-01 17:28:29 +0200

    occasional commit

--- Cpp_Course/main.cpp
+++ Cpp_Course/main.cpp
@@ -2,23 +2,9 @@
 
 using namespace std;
 
-
 int main()
 {
-    int index = 1;
 
-    while(index <= 5){
-        cout << index << endl;
-        index++;
-    }
-
-    cout << "\n" << endl;
-
-    int index1 = 1;
-    do{
-        cout << index1 << endl;
-        index1++;
-    }while(index1 <=5);
 
     return 0;
 }
